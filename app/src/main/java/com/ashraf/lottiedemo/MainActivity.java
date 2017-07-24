c1a074838724cc715595851334c29e7fced6fac0
Ashraf Patel <ashrafp725@gmail.com>
2017-07-24 15:31:53 +0530

    Initial commit

---
+++ app/src/main/java/com/ashraf/lottiedemo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.ashraf.lottiedemo;
+
+import android.animation.Animator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LottieAnimationView animationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        animationView = (LottieAnimationView) findViewById(R.id.animation_view);
+
+        Button btn = (Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animationView.setAnimation("sample.json");
+                animationView.loop(false);
+                animationView.playAnimation();
+            }
+        });
+    }
+}
